c509eb186273bd7d9324aa8288a616d26acece6a
FireCodingDay <69696645+parthuarnax@users.noreply.github.com>
2021-07-04 14:47:30 +0900

    kiosk mode complete

--- app/src/main/java/com/game/dilemma/Riverside_main.java
+++ app/src/main/java/com/game/dilemma/Riverside_main.java
@@ -1,7 +1,14 @@
 package com.game.dilemma;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.gesture.GestureOverlayView;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
@@ -19,6 +26,7 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -28,6 +36,7 @@ import android.widget.VideoView;
 
 import org.w3c.dom.Text;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.EventListener;
 
@@ -42,6 +51,61 @@ import android.widget.ScrollView;
 import android.widget.TextView;import android.media.AudioManager;import android.media.SoundPool;
 
 public class Riverside_main extends AppCompatActivity {
+
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LOW_PROFILE |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE
+        );
+    }
+
+    private void showSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
+    }
+
+    private final Handler mHideHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            hideSystemUI();
+        }
+    };
+
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeMessages(0);
+        mHideHandler.sendEmptyMessageDelayed(0, delayMillis);
+    }
+
+    //시스템 다이얼로그 닫기 로직
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        // When the window loses focus (e.g. the action overflow is shown),
+        // cancel any pending hide action. When the window gains focus,
+        // hide the system UI.
+        if (hasFocus) {
+            delayedHide(300);
+        } else {
+            mHideHandler.removeMessages(0);
+        }
+
+        Log.d("Focus debug", "Focus changed !");
+
+        if(!hasFocus) {
+            Log.d("Focus debug", "Lost focus !");
+
+            Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+            sendBroadcast(closeDialog);
+        }
+    }
+
     //객체 선언
     View touchArea;
     boolean touchActive = false;
@@ -380,21 +444,17 @@ public class Riverside_main extends AppCompatActivity {
 
     int kill_cnt = 0;
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.riverside_main);
-        kill_btn = (Button)findViewById(R.id.kill_btn);
-        kill_btn.setOnTouchListener(new View.OnTouchListener(){
-            @Override
-            public boolean onTouch(View v, MotionEvent event){
-                kill_cnt++;
-                if(kill_cnt > 200){
-                    finish();
-                }
-                return true;
-            }
-        });
+        this.startLockTask();
+
+        //status bar는 hide
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         MySoundPlayer.initSounds(getApplicationContext());
 
         decorView = getWindow().getDecorView();
@@ -406,6 +466,20 @@ public class Riverside_main extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
             uiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         decorView.setSystemUiVisibility(uiOption);
+
+        kill_btn = findViewById(R.id.kill_btn);
+        kill_btn.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event){
+                kill_cnt++;
+                Log.d("cnt",Integer.toString(kill_cnt));
+                if(kill_cnt > 300){
+                    stopLockTask();
+                    finish();
+                }
+                return true;
+            }
+        });
 
         //A4 장면의 '중', '지' 각 버튼
         //A4 '중' '지' 동시터치 체크
@@ -473,12 +547,12 @@ public class Riverside_main extends AppCompatActivity {
                     //printString("손가락 눌림 : " + curX + ", " + curY);
                 } else if (action == event.ACTION_MOVE) {    //누르고 움직였을 때
                     //printString("손가락 움직임 : " + curX + ", " + curY);
-                    if (curX < 390 || curX > 505) {
+                    if (curX < 390 || curX > 605) {
                         isStraight = false;
                     }
                 } else if (action == event.ACTION_UP) {    //누른걸 뗐을 때
                     //printString("손가락 뗌 : " + curX + ", " + curY);
-                    boolean isLong = curY - raindrop_start > 200;
+                    boolean isLong = curY - raindrop_start > 100;
                     if (isLong && isStraight && narr_end[2]) {
                         narr_end[3] = true;
                         controlpanel_anim();
